4aa0229ced14e2358d5271b1fc699697d05ee729
Abhishek <abhishekc838@gmail.com>
2021-10-04 13:37:17 +0530

    HotStar

--- js/data.js
+++ js/data.js
@@ -3,35 +3,35 @@ let movies = [
         name: 'The falcon and the winter soldier',
         moreInfo: '1 Season • 6 Episodes • Superhero • U/A 13+ • Marvel',
         des: 'Following the events of "Avengers: Endgame", Sam Wilson and Bucky Bames team up in a global adventure that tests theis abilites and their patience',
-        image: 'images/slider 1.png',
+        image: 'images/slider1.png',
         watch: '▶️ Watch Movie'
     },
     {
         name: 'loki',
         moreInfo: '1 Season • 6 Episodes • Superhero • U/A 13+ • Marvel',
         des: 'The mercurial villain Loki resume his role as the God of Mischief in a new series that takes place after the events of "Avengers: Endgame"',
-        image: 'images/slider 2.png',
+        image: 'images/slider2.png',
         watch: '▶️ Watch Movie'
     },
     {
         name: 'Wanda Vision',
         moreInfo: '1 Season • 9 Episodes • Superhero • U/A 13+ • Marvel',
         des: 'Wanda Maximoff and  Vision-two-super-powered beings living idealized suburban lives-begin to suspect that everything is not as it seems',
-        image: 'images/slider 3.png',
+        image: 'images/slider3.png',
         watch: '▶️ Watch Movie'
     },
     {
         name: 'raya and the last dragon',
         moreInfo: '1 hr 47 min • 2021 • Animation • U/A 7+',
         des: 'Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world',
-        image: 'images/slider 4.png',
+        image: 'images/slider4.png',
         watch: '▶️ Watch Movie'
     },
     {
         name: 'luca',
         moreInfo: '1 hr 35 min • 2021 • Animation • U/A7+',
         des: 'The movie is a coming-of-age story about young boy experiencing an unforgettable summer filles with gelato, pasta and endless scooter rides.',
-        image: 'images/slider 5.png',
+        image: 'images/slider5.png',
         watch: '▶️ Watch Movie'
     }
 ]
